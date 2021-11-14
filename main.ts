5ba72185c70c2aa5c5cb8149c4e0f72a883b342b
Andrei Kosach <andreikosach@Andreis-MacBook-Pro.local>
2021-11-14 13:28:19 +0200

    Added hot reload

--- main.ts
+++ main.ts
@@ -1,14 +1,1 @@
-
-import {SocialNetwork} from "./social-network";
-import "./interfaceVSType";
-import "/generics";
-
-class App implements SocialNetwork {
-  title = "Test";
-  getUsers(){
-    return [{name: "Jhon", age: '12'}];
-  }
-}
-
-const test = new App();
-// console.log(test.getUsers());
+console.log('Started playground');
